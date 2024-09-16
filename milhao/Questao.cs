17267d75b4f775a993a0b417a30f2d96f1cee1ef
luquitas <viniciaosantosdacosta37378@gmail.com>
2024-09-16 09:05:12 -0300

    aauahuiasiUUIC

---
+++ milhao/Questao.cs
@@ -0,0 +1,33 @@
+namespace milhao;
+
+public class Questao
+{
+    public Questao()
+    {
+    
+    }
+    
+    public string? Texts;
+
+    private int? difficulty;
+    
+    public bool CorrectAnswer;
+
+
+    public Questao()
+    {
+    
+    }
+
+    private string? Questions;
+
+    private string? Answer01;
+
+    private string? Answer02;
+
+    private string? Answer03;
+
+    private string? Answer04;
+
+    private string? Answer05;
+}
